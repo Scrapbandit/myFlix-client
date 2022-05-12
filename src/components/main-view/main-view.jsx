ef247c025268b26e671e986b44721daccd62a944
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-05-12 21:23:06 +0200

    registration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -9,8 +10,9 @@ export class MainView extends React.Component{
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
-    }
+      selectedMovie: null,
+      user: null
+    };
   }
 
 
@@ -26,15 +28,22 @@ componentDidMount(){
     });
 }
 
-setSelectedMovie(newSelectedMovie) {
+setSelectedMovie(movie) {
   this.setState({
-    selectedMovie: newSelectedMovie
+    selectedMovie: movie
+  });
+}
+
+onLoggedIn(user) {
+  this.setState({
+    user
   });
 }
 
 render() {
-  const { movies, selectedMovie } = this.state;
+  const { movies, selectedMovie, user } = this.state;
 
+  if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
   if (movies.length === 0) return <div className="main-view"/>;
 
@@ -43,7 +52,7 @@ render() {
       {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
         ))
       }
     </div>
