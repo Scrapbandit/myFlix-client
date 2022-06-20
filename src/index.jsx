7edb37fbecfcdc87c92456b287073e9c6e5bc5b2
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-06-20 12:40:09 +0200

    unlinked netlify

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
-import { createStore } from 'redux';
+import { createStore } from 'redux'; 
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
 import MainView from './components/main-view/main-view';
